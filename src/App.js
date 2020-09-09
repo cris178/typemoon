8faec1970cd889c14b2291320300a759aefcd1e7
Cristian <cris178@outlook.com>
2020-09-09 00:58:20 -0700

    It's responsive now

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useEffect,useState} from 'react';
 import './App.css';
-import Submit from './components/submit/submit'
+import Submit from './components/submit/submit';
+
+
+//import two apis API for communicating with appsync API.graohql
+//graphQL operation lets us wrap queries below it and wrap it in a function
+import Amplify, { API, graphqlOperation } from 'aws-amplify'
+import { createPost } from './graphql/mutations'
+import { listPosts } from './graphql/queries'
+
+import awsExports from "./aws-exports";
+import { getDefaultNormalizer } from '@testing-library/react';
+Amplify.configure(awsExports);
+
+
+
+//Useeffect is essentially component did mount
+//UseState is to use state
 
 function App() {
+  //We're going to set posts to an empty array in our state
+  const [posts, updatedPosts] = useState([]);
+
+  useEffect(()=>{
+    getData()
+  },[]);
+
+  async function getData(){
+    try{
+      const postData = await API.graphql(graphqlOperation(listPosts));
+      console.log('postData: ' + postData);
+      let info = Object.keys(postData);
+      console.log(postData.data);
+      updatedPosts(postData.data.listPosts.items);
+
+    }catch(err){
+      console.log("Error fetching posts..." + err);
+    }
+
+  }
+
+
   return (
     <div className="App">
      <nav>
--- src/components/submit/submit.js
+++ src/components/submit/submit.js
@@ -3,12 +3,34 @@ import './submit.css';
 
 
 class Submit extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state ={
+            submission: ""
+        }
+        this.handleTermChange = this.handleTermChange.bind(this);
+        this.handleSubmission = this.handleSubmission.bind(this);
+    }
+
+
+    handleTermChange(event){
+        console.log(event.target.value);
+        this.setState({
+            submission:event.target.value
+        })
+    }
+
+    handleSubmission(){
+
+    }
+
     render(){
         return(
             <div className="Submit border-left border-right">
                 <div className="submitPost">
-                <input type="text" className="post" placeholder="What's happening?" /> 
-                <button className="submitButton">Submit</button>
+                <input onChange={this.handleTermChange} type="text" className="post" placeholder="What's happening?" /> 
+                <button onClick={this.handleSubmission} className="submitButton">Submit</button>
                 <hr></hr>
                 </div>
             </div>
