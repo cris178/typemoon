9819df6d3e76dedd92f35893ebbbd35e5b4712a2
Cristian <cris178@outlook.com>
2020-09-28 00:05:43 -0700

    Building CI

--- src/components/posts/posts.js
+++ src/components/posts/posts.js
@@ -2,13 +2,7 @@ import React from 'react';
 import './Posts.css'
 
 class Posts extends React.Component{
-    constructor(props){
-        super(props);
-        this.state ={
-            
-        }
-
-    }
+    
     render(){
         console.log("Data: " + this.props.date);
         return(
